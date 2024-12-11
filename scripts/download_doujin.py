2bbda394a87004439f1a1f42163b86c03548738a
Anindya-ctrl <and_@tutanota.com>
2024-12-11 16:09:31 +0600

    [script] improved nhentai script to include its name in the zip file instead of id

--- scripts/download_doujin.py
+++ scripts/download_doujin.py
@@ -3,20 +3,37 @@ import zipfile
 import requests
 from bs4 import BeautifulSoup
 
+# process input and fetch page
 id = input("Enter doujin ID: ")
 url = f"https://nhentai.net/g/{id}/"
 res = requests.get(url)
 
 if res.status_code == 200:
-    print("loaded successfully")
+    print("Loaded successfully.")
 else:
     print(f"ERROR LOADING SITE: {res.status_code}")
 
 soup = BeautifulSoup(res.content, "html.parser")
 
-pattern = re.compile(r"https://t(\d+)\.nhentai\.net/galleries/\d+/(\d+)t\.jpg")
+# get title
+doujin_left = soup.find("span", class_="before")
+doujin_middle = soup.find("span", class_="pretty")
+doujin_right = soup.find("span", class_="after")
+if doujin_left and doujin_middle and doujin_right:
+    doujin_title = (
+        doujin_left.get_text(strip=True)
+        + " "
+        + doujin_middle.get_text(strip=True)
+        + " "
+        + doujin_right.get_text(strip=True)
+    )
+else:
+    doujin_title = id
 
+# get image links
+pattern = re.compile(r"https://t(\d+)\.nhentai\.net/galleries/\d+/(\d+)t\.jpg")
 doujin = []
+
 for img in soup.find_all("img"):
     sauce = img.get("src")
 
@@ -25,19 +42,19 @@ for img in soup.find_all("img"):
         sauce = re.sub(r"(\d+)t\.jpg", r"\1.jpg", sauce)
         doujin.append(sauce)
 
-for r in doujin:
-    print(r)
-print("PAGE COUNT:", len(doujin))
+print(f"Page count: {len(doujin)}.")
+print(f"Saving into: {doujin_title}.zip.")
 
-with zipfile.ZipFile(f"{id}.zip", "w", zipfile.ZIP_DEFLATED) as zf:
+# download
+with zipfile.ZipFile(f"{doujin_title}.zip", "w", zipfile.ZIP_DEFLATED) as zf:
     for i, img_url in enumerate(doujin):
         img_res = requests.get(img_url)
 
         if img_res.status_code == 200:
             img_name = img_url.split("/")[-1]
             zf.writestr(img_name, img_res.content)
-            print(f"done with {img_name}")
+            print(f"Done with {img_name}...")
         else:
-            print(f"something went wrong with {img_url}")
+            print(f"Something went wrong with {img_url}...")
 
 print("all done")
