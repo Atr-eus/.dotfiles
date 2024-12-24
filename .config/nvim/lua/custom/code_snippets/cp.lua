aad54d6634998cabdac9d0075157d090e0891947
Anindya-ctrl <and_@tutanota.com>
2024-12-24 19:08:12 +0600

    [neovim] added snippet support

---
+++ .config/nvim/lua/custom/code_snippets/cp.lua
@@ -0,0 +1,33 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("cpp", {
+  s("cp", fmt([[
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void i_love_emilia() {{
+{}{}
+}}
+
+int main() {{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--) {{
+    i_love_emilia();
+  }}
+}}
+]], {
+    t("  "),
+    i(1, "// Who is rem?"),
+  })
+  ),
+})
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,11 +3,10 @@ return {
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
   "jay-babu/mason-null-ls.nvim",
-  "jose-elias-alvarez/null-ls.nvim",
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets" },
-  },
+  "nvimtools/none-ls.nvim",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets",
   {
     "williamboman/mason.nvim",
     config = function()
@@ -19,8 +18,15 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local lsp_defaults = lspconfig.util.default_config
+      local cmp = require("cmp")
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local luasnip = require("luasnip")
+      require("luasnip.loaders.from_lua").load({ paths = "./lua/custom/code_snippets" })
+      require("luasnip.loaders.from_vscode").load()
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
       lsp_defaults.capabilities = vim.tbl_deep_extend("force", lsp_defaults.capabilities, capabilities)
 
       require("mason-lspconfig").setup({
@@ -32,7 +38,6 @@ return {
           "clangd",
           "pyright",
           "tailwindcss",
-          "mdx_analyzer",
           "ts_ls",
         },
         handlers = {
@@ -44,15 +49,6 @@ return {
         },
       })
 
-      -- band-aid for multiple encoding warn
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = {
-          "clangd",
-          "--offset-encoding=utf-16",
-        },
-      })
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(ev)
           vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
@@ -69,46 +65,57 @@ return {
           --end, opts)
         end,
       })
-      local cmp = require("cmp")
-      local ls = require("luasnip")
 
       cmp.setup({
-        sources = {
-          { name = "nvim_lsp" },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
         snippet = {
           expand = function(args)
-            ls.lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
-        window = {},
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+        mapping = {
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-g>"] = cmp.mapping.complete(),
+          ["<C-l>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
       })
-      local s = ls.snippet
-      local t = ls.text_node
-      local i = ls.insert_node
-      ls.add_snippets("lua", {
-        s("hello", {
-          t('print("hello, '),
-          i(1),
-          t('")'),
-        }),
-      })
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/custom/code_snippets" } })
+
 
       require("mason-null-ls").setup({
         ensure_installed = { "black", "latexindent" },
       })
 
       local null_ls = require("null-ls")
-
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.prettier,
         },
+      })
+
+      vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
       })
     end,
   },
