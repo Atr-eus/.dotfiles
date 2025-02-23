dfa084dec4b4571183d24c77dc17b43d62c8e979
Anindya-ctrl <and_@tutanota.com>
2025-02-23 20:57:34 +0600

    [fish] remove stupid custom prompt because it has a bug

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
 if status is-interactive
   set -U fish_greeting ""
   fish_vi_key_bindings
+  
+  set -g -x JAVA_HOME '/usr/lib/jvm/java-23-openjdk/'
+  set -g -x PATH_TO_FX '/home/and/.openjfx/javafx-sdk-23.0.2/lib'
 
-  function fish_prompt
-    set_color green
-    echo -n (prompt_pwd)
-    set_color normal
-    echo -n '> '
-  end
+  # function fish_prompt
+  #   set_color green
+  #   echo -n (prompt_pwd)
+  #   set_color normal
+  #   echo -n '> '
+  # end
 end
