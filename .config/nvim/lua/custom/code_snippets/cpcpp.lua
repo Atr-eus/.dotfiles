daf7f4ad734fbb7a795125a6d5374b4afcffccd2
Anindya-ctrl <and_@tutanota.com>
2025-04-02 10:20:23 +0600

    [neovim] removed meme from CP code snippets

--- .config/nvim/lua/custom/code_snippets/cpcpp.lua
+++ .config/nvim/lua/custom/code_snippets/cpcpp.lua
@@ -10,7 +10,7 @@ ls.add_snippets("cpp", {
 using namespace std;
 typedef long long ll;
 
-void i_love_emilia() {{
+void an() {{
 {}{}
 }}
 
@@ -22,7 +22,7 @@ int main() {{
   int t;
   cin >> t;
   while (t--) {{
-    i_love_emilia();
+    an();
   }}
 }}
 ]], {
--- .config/nvim/lua/custom/code_snippets/cppy.lua
+++ .config/nvim/lua/custom/code_snippets/cppy.lua
@@ -7,15 +7,16 @@ local fmt = require("luasnip.extras.fmt").fmt
 ls.add_snippets("python", {
   s("cppy", fmt([[
 import sys
+
 input = sys.stdin.readline
 
-def i_love_emilia():
+def an():
 {}{}
 
 if __name__ == "__main__":
     t = int(input().strip())
     for _ in range(t):
-        i_love_emilia()
+        an()
 ]], {
     t("    "),
     i(1, "# Who is rem?"),
